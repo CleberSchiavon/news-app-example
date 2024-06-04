a9ea3d04e1f8fbb8347cdb50f9715ffba7b5241a
Cleber Henrique <cleberschiavon@outlook.com>
2024-06-04 04:03:41 -0300

    feat(app): creating husky and conventional commit check

---
+++ scripts/husky.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+echo "INSTALLING HUSKY..."
+
+rm -fr .husky
+pnpm husky install
+pnpm husky add .husky/pre-push "pnpm run lint && pnpm run changelog"
+pnpm husky add .husky/commit-msg "npx commitlint --edit $1"
+
+echo "HUSKY INSTALLED"
