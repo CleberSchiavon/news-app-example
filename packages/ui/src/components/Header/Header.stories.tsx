8429775ea18c030d24c23b605a13b47c6a2c340b
Cleber Henrique <cleberschiavon@outlook.com>
2024-06-23 22:14:56 -0300

    refactor(app): running linters

--- packages/ui/src/components/Header/Header.stories.tsx
+++ packages/ui/src/components/Header/Header.stories.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import { Meta, StoryFn } from '@storybook/react'
+import { Meta, StoryFn } from "@storybook/react";
 
 import { FiArrowLeft } from "react-icons/fi";
 
-
-import {HeaderComponent} from './index'
+import { HeaderComponent } from "./index";
 
 export default {
-  title: 'Components/Header',
+  title: "Components/Header",
   component: HeaderComponent,
-} as Meta<typeof HeaderComponent>
+} as Meta<typeof HeaderComponent>;
 
-export const Default: StoryFn<typeof HeaderComponent> = (args) => <HeaderComponent {...args} navText="News App" />
+export const Default: StoryFn<typeof HeaderComponent> = (args) => (
+  <HeaderComponent {...args} navText="News App" />
+);
 
-export const WithIcon: StoryFn<typeof HeaderComponent> = (args) => <HeaderComponent {...args} icon={<FiArrowLeft color="white" size={20} />} onClickIcon={() => console.log('Oi')} navText="News App" />
+export const WithIcon: StoryFn<typeof HeaderComponent> = (args) => (
+  <HeaderComponent
+    {...args}
+    icon={<FiArrowLeft color="white" size={20} />}
+    onClickIcon={() => console.log("Oi")}
+    navText="News App"
+  />
+);
--- packages/ui/src/components/Header/index.tsx
+++ packages/ui/src/components/Header/index.tsx
@@ -1,40 +1,41 @@
-import React, { ReactElement } from 'react'
-import './styles.scss'
+import React, { ReactElement } from "react";
+import "./styles.scss";
 
 interface IHeaderComponent {
-
   /**
- * @param {string} navText - Nav Text
- *
- */
+   * @param {string} navText - Nav Text
+   *
+   */
   navText: string;
 
   /**
-* @param {ReactElement} icon - Icon Component
-*
-*/
+   * @param {ReactElement} icon - Icon Component
+   *
+   */
   icon?: ReactElement;
 
-/**
-* @param {void} onClickIcon - On Click Icon Function
-*
-*/
-onClickIcon?: () => void;
-
-
+  /**
+   * @param {void} onClickIcon - On Click Icon Function
+   *
+   */
+  onClickIcon?: () => void;
 }
-export function HeaderComponent({ navText, icon, onClickIcon }: IHeaderComponent) {
-  const HeaderIcon = icon
+export function HeaderComponent({
+  navText,
+  icon,
+  onClickIcon,
+}: IHeaderComponent) {
+  const HeaderIcon = icon;
   return (
     <nav className="main-nav">
-        {HeaderIcon && (
-          <div className="button-icon" onClick={onClickIcon}>
-            {HeaderIcon}
-          </div>
-        )}
-        <div className="nav-text">
-          <p>{navText}</p>
+      {HeaderIcon && (
+        <div className="button-icon" onClick={onClickIcon}>
+          {HeaderIcon}
         </div>
+      )}
+      <div className="nav-text">
+        <p>{navText}</p>
+      </div>
     </nav>
-  )
+  );
 }
